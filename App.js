c1177ccd38c0d5d7ed553329e9508324c7d58cd3
jawish-a@outlook.com <jawish-a@outlook.com>
2020-11-21 11:54:33 +0300

    added fonts to fix the red error roboto fonts

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Container } from "native-base";
+//
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
 // Navigation
 import RootNavigator from "./Navigation";
 import { NavigationContainer } from "@react-navigation/native";
@@ -11,6 +15,19 @@ import { Provider } from "react-redux";
 import store from "./redux";
 
 export default function App() {
+  // const [loading, setLoading] = useState(true)
+
+  const [loaded] = Font.useFonts({
+    'Roboto': require('native-base/Fonts/Roboto.ttf'),
+    'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+  });
+
+  if (!loaded) {
+    return <AppLoading />
+  }
+
+
+
   return (
     <Provider store={store}>
       <NavigationContainer>
