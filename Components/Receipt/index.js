18137c253dec8364df5b38f0c973743e01247e7b
ghadeer <apps02977@gmail.com>
2020-11-29 15:21:33 +0300

    add receipt and edit in the card operation

---
+++ Components/Receipt/index.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { connect } from "react-redux";
+import ItemReceipt from "./ItemReceipt";
+import {
+  Text,
+  Left,
+  Body,
+  Content,
+  Card,
+  CardItem,
+  Container,
+  Button,
+} from "native-base";
+
+import { Col, Row } from "react-native-easy-grid";
+
+import { setCart } from "../../redux/actions";
+
+const Receipt = ({ cart, navigation, user, setCart }) => {
+  let items = cart.items;
+  const itemsReceipt = items.map((item) => (
+    <ItemReceipt key={item.id} item={item} />
+  ));
+
+  console.log("itemsReceipt");
+
+  console.log(itemsReceipt);
+
+  const handleReturn = () => {
+    setCart();
+    navigation.replace("Cart");
+  };
+
+  return (
+    <Container>
+      <Content>
+        <Card style={{ flex: 0 }}>
+          <CardItem header>
+            <Row>
+              <Text>First Name : {user?.first_name}</Text>
+
+              <Text>Last Name : {user?.last_name}</Text>
+            </Row>
+            <Row>
+              <Text>Number: {user?.username}</Text>
+            </Row>
+            <Row>
+              <Text>Email : {user?.email}</Text>
+            </Row>
+            <Left>
+              <Body>
+                {items.length ? (
+                  <>{itemsReceipt}</>
+                ) : (
+                  <Text>Receipt is empty</Text>
+                )}
+              </Body>
+            </Left>
+          </CardItem>
+          <CardItem header>
+            <Left>
+              <Body>
+                <Text>Subtotal: {cart.subtotal}</Text>
+                <Text>Tax(15%): {cart.tax}</Text>
+                <Text>Total: {cart.total}</Text>
+
+                <Button onPress={() => handleReturn()}>
+                  <Text> return to cart</Text>
+                </Button>
+              </Body>
+            </Left>
+          </CardItem>
+        </Card>
+      </Content>
+    </Container>
+  );
+};
+const mapStateToProps = ({ cart, user }) => ({
+  cart,
+  user,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setCart: () => dispatch(setCart()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Receipt);
--- redux/actions/payment.js
+++ redux/actions/payment.js
@@ -2,15 +2,15 @@ import { SET_PAYMENT } from "./actionTypes";
 import instance from "./instance";
 
 export const fetchPaymentLink = (orderNumber) => async (dispatch) => {
-    try {
-        const res = await instance.get(`checkout/${orderNumber}/get_payment_url/`);
-        const paymnetLink = res.data;
+  try {
+    const res = await instance.post(`checkout/${orderNumber}/get_payment_url/`);
+    const paymnetLink = res.data;
 
-        dispatch({
-            type: SET_PAYMENT,
-            payload: paymnetLink,
-        });
-    } catch (error) {
-        console.error(error);
-    }
+    dispatch({
+      type: SET_PAYMENT,
+      payload: paymnetLink,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
